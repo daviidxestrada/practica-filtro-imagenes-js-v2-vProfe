bbc62611aa21bbf048da350d3327505b7e63f565
unknown <daviidxestrada@gmail.com>
2025-10-01 10:59:58 +0200

    Ejercicio Solucionado con Profe

--- js/main.js
+++ js/main.js
@@ -1,94 +1,106 @@
-const fotos = [
-    // Amigos
-    { src: "assets/imgs/amigos-01.jpg", alt: "polo en plaza mayor", title: "Amigos 1", tags: ["amigos"]},
-    {  src: "assets/imgs/amigos-02.jpg", alt: "alvarito sexy", title: "Amigos 2", tags: ["amigos"] },
+const botonera = document.querySelector("#botonera")
+const imgContainer = document.querySelector("#imgContainer")
 
-    // Dibujos
-    { src: "assets/imgs/dibujos-01.jpg", alt:"La Calabaza Charlie Brown", title: "Dibujos 1", tags: ["dibujos"]},
-    { src: "assets/imgs/dibujos-02.jpg", alt: "Over the Garden Wall", title: "Dibujos 2", tags: ["dibujos"] },
 
-    // Naturaleza 
-    { src: "assets/imgs/naturaleza-01.jpg", alt: "La playa", title: "Naturaleza 1", tags:["naturaleza"] },
-    { src: "assets/imgs/naturaleza-02.jpg", alt: "Edificios Ramen", title: "Naturaleza 2", tags: ["naturaleza"] },
 
-    // Nana
-    { src: "assets/imgs/nana-01.jpg", alt: "Nana comiendo ramen", title: "Nana 1", tags: ["nana"] },
-    { src: "assets/imgs/nana-02.jpg", alt: "Nana guapeando", title: "Nana 2", tags: ["nana"] },
-];
+const botones = ["amigos", "dibujos","nana","naturaleza"]
 
-// Seleccionamos el contenedor donde irán las imágenes
-const galeria = document.querySelector("#galeria")
-
-const contador = document.querySelector("#contador");
-
-//Función que pinta fotos en la galería
-function pintarGaleria(lista){
-    galeria.innerHTML = ""; // vaciamos antes de pintar
-    
-    lista.forEach((foto) => {
-        const figure = document.createElement("figure");
-
-        const img = document.createElement("img");
-        img.src = foto.src;
-        img.alt = foto.alt;
-
-        const caption = document.createElement("figcaption");
-        caption.textContent = foto.title;
-
-        figure.appendChild(img);
-        figure.appendChild(caption);
-        galeria.appendChild(figure);
-    });
-
-      // Actualizar el contador
-      contador.textContent = `Se muestran ${lista.length} fotos`;
-
-    }
-
-    //Probar: pinta todas las fotos al cargar
-
-    pintarGaleria(fotos);
-
-console.log("Total fotos:", fotos.length);
-
-const filtros = document.querySelector("#filtros")
-
-const categorias = [
-    {slug: "amigos", label:"Amigos"},
-    {slug: "dibujos", label:"Dibujos"},
-    {slug: "naturaleza", label:"Naturaleza"},
-    {slug: "nana", label:"nana<3"},
-    {slug: "todos", label:"Todos"} // para resetear
+const arrayFotos = [
+    {
+        id:1,
+        url:"assets/imgs/amigos-01.jpg",
+        alt: "Amigos 1",
+        tag: ["amigos"],
+     },
+     {
+        id:2,
+        url:"assets/imgs/amigos-02.jpg",
+        alt: "Amigos 2",
+        tag: ["amigos"],
+     },
+     {
+        id:3,
+        url:"assets/imgs/dibujos-01.jpg",
+        alt: "Dibujos 1",
+        tag: ["dibujos"],
+     },
+     {
+        id:4,
+        url:"assets/imgs/dibujos-02.jpg",
+        alt: "Dibujos 2",
+        tag: ["dibujos"],
+     },
+     {
+        id:5,
+        url:"assets/imgs/nana-01.jpg",
+        alt: "Nana 1",
+        tag: ["nana"],
+     },
+     {
+        id:6,
+        url:"assets/imgs/nana-02.jpg",
+        alt: "Nana 2",
+        tag: ["nana"],
+     },
+     {
+        id:7,
+        url:"assets/imgs/naturaleza-01.jpg",
+        alt: "Naturaleza 1",
+        tag: ["naturaleza"],
+     },
+     {
+        id:8,
+        url:"assets/imgs/naturaleza-02.jpg",
+        alt: "Naturaleza 2",
+        tag: ["naturaleza"],
+     },
+     
+     
 ]
 
-//pintar los botones
-categorias.forEach((cat) => {
-    const boton = document.createElement("button");
-    boton.textContent = cat.label
-    boton.dataset.filtro = cat.slug // atributo personalizado
-    filtros.appendChild(boton)
-});
 
-filtros.addEventListener("click",(ev)=>{
-    if(ev.target.tagName === "BUTTON"){
-        const filtro = ev.target.dataset.filtro;
-
-    document.querySelectorAll("#filtros button").forEach((btn)=>{
-        btn.classList.remove("activo");
-    }
-)
-
-   ev.target.classList.add("activo")
-
-        if (filtro === "todos") {
-            pintarGaleria(fotos);
-            }else {
-                const filtradas = fotos.filter((foto) => foto.tags.includes(filtro));
-                pintarGaleria(filtradas);
-            }
+document.addEventListener("click",(ev)=>{
+    if(ev.target.matches("#botonera button")){
+        const id=ev.target.id
         
+        filtrarImagenes(id)
     }
 })
 
 
+const pintarBotones = ()=>{
 
+botones.forEach((boton)=>{
+    botonera.innerHTML +=`<button id="${boton}">${boton}</button>`
+})
+
+}
+
+const filtrarImagenes = (id)=>{
+      console.log(id,"en filtrar imagen")
+
+     const filtradas = arrayFotos.filter((objeto)=>{
+        return objeto.tag.includes(id)
+     })
+     pintarImagenes(filtradas)
+    }
+
+const pintarImagenes = (array) => {
+    const fragment=document.createDocumentFragment()
+    imgContainer.innerHTML=""
+   
+     array.forEach((imagen)=>{
+        const card = document.createElement("article")
+        const foto = document.createElement("img")
+        foto.src=imagen.url
+        foto.alt=imagen.alt
+        
+        card.append(foto)
+        fragment.append(card)
+     })
+     
+     imgContainer.append(fragment)
+}
+
+pintarBotones()
+pintarImagenes(arrayFotos)
